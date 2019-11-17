5374e20c064d7d536c5b10b6626fa1dd88fe052f
noa21-meet <52985362+noa21-meet@users.noreply.github.com>
2019-11-17 18:51:42 +0200

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -1,5 +1,5 @@
 class User(object):
-	def __init__ (self, name, email, password)
+	def __init__ (self, name, email, password):
 		self.name = name
 		self.email = email
 		self.password = password
@@ -14,27 +14,43 @@ class User(object):
 		self.friends_list.remove(email)
 		print(self.name + "has removed " + email + "as a friend.")
 
-	def post(self, text):
-		self.post.append(text)
-		print(self.name + "has posted " + post)
+	def add_post(self, post_text, date):
+		post1 =post(date. text)
+		self.date = date
+		self.text = post_text
+		author = self.name
+		self.posts.append(text)
+		print(self.name + "has posted " +text)
 
-	def "get_userInfo"(self):
-		print ("Name:["+self.name+"]"+"email: ["+self.email+"]"+"password: ["+self.password+"]+friends: ["+str.friends_list+"]+'posts: ["+self.posts+"]")
+	def get_userInfo(self):
+		print ("Name:["+self.name+"]"+"email: ["+self.email+"]"+"password: ["+self.password+"]+friends: ["+str(self.friends_list)+"]+'posts: ["+str(self.posts)+"]")
 
-class Post(object):
-	def __init__(self, ):
-		super(Post, self).__init__()
-		self.arg = arg
-		
+class Post(User):
+	def __init__(self, post_text,date):
+		self.post_text=post_text
+		self.comment = []
+		self.date = date
 
-user1 = User("Eyal","eyal@meet.mit.edu", "123")	
+			def create_comment(self, comment_text):
+				self.comments.append(comment_text)
+				print("your comment" + self.comment_text)
+
+			def remove_comment(self, comment_text):
+				self.comments.remove(comment_text)
+				print("you removed your comment" + comment_text)
+
+			def edit_comment(self, comment_text):
+				self.comment_text=new_comment
+				print("your comment has been changed")
+
+			def post_date(self):
+				print("posted on" + self.post_date)	
+
+user1 = User('Eyal','eyal@meet.mit.edu', '123')	
 user2 = User("Ariel","ariel@meet.mit.edu", "123")
 user2.add_friend("eyal@meet.mit.edu")
-user1.post('Hello')
+user1.add_post('Hello')
 user1.get_userInfo()
 user2.get_userInfo()
 user2.remove_friend("eyal@meet.mit.edu")
-
-
-
 
