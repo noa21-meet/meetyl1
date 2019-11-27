7a7bd58b6c6bbca6dba473882f02926e1e8e9159
noa21-meet <52985362+noa21-meet@users.noreply.github.com>
2019-11-27 18:44:19 +0200

    Add files via upload

--- facebook.py
+++ facebook.py
@@ -1,3 +1,23 @@
+users=[]
+posts=[]
+comments=[]
+
+username='noa'
+password='123'
+
+random=0
+
+while random==0:
+	try_username=input('what is your username?')
+	try_password=input('what is your password')
+
+	if try_username==username and try_password==password:
+		random=1
+		print('welcome!!!')
+
+	else:
+		print('try again please')	
+
 class User(object):
 	def __init__ (self, name, email, password):
 		self.name = name
@@ -5,6 +25,8 @@ class User(object):
 		self.password = password
 		self.friends_list =[] 
 		self.posts= []
+		self.new_messages=[]
+		users.append(self)
 
 	def add_friend(self,email):
 		self.friends_list.append(email)
@@ -25,26 +47,44 @@ class User(object):
 	def get_userInfo(self):
 		print ("Name:["+self.name+"]"+"email: ["+self.email+"]"+"password: ["+self.password+"]+friends: ["+str(self.friends_list)+"]+'posts: ["+str(self.posts)+"]")
 
-class Post(User):
+	def  send_d(self):
+		reciver=input('Direct Messages with...?')
+		message_text=input('enter a message')
+		for i in users:
+			if reciver in self.name:
+				reciver.append(message_text)		
+
+class post:	
 	def __init__(self, post_text,date):
 		self.post_text=post_text
-		self.comment = []
 		self.date = date
 
-			def create_comment(self, comment_text):
-				self.comments.append(comment_text)
-				print("your comment" + self.comment_text)
+		def post(self):
+			posts.append(post_text)
 
-			def remove_comment(self, comment_text):
-				self.comments.remove(comment_text)
-				print("you removed your comment" + comment_text)
+		def post_date(self):
+			print('posted on' +self.date)
 
-			def edit_comment(self, comment_text):
-				self.comment_text=new_comment
-				print("your comment has been changed")
 
-			def post_date(self):
-				print("posted on" + self.post_date)	
+	class Comment(Post):
+		def __init__ (self,comment_text):
+			self.comment_text=comment_text
+
+		def create_comment(self):
+			comments.append(self.comment_text)
+			print('your comment is '+ self.comment_text )
+
+		def remove_comment(self, comment_text):
+			comments.remove(comment_text)
+			print("you removed your comment" + comment_text)
+
+		def edit_comment(self, comment_text):
+			self.comment_text=new_comment
+			print("your comment has been changed")
+
+class Messages(user):
+	def __init__(self,message_text):
+		self.message_text=message_text
 
 user1 = User('Eyal','eyal@meet.mit.edu', '123')	
 user2 = User("Ariel","ariel@meet.mit.edu", "123")
@@ -53,4 +93,3 @@ user1.add_post('Hello')
 user1.get_userInfo()
 user2.get_userInfo()
 user2.remove_friend("eyal@meet.mit.edu")
-
